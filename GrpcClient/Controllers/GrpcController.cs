cb69738bb39db0fc3333ecbfc1ddd5f336269121
Pashaie <pashaie@gmail.com>
2025-03-10 21:05:23 +0330

    grpc client

---
+++ GrpcClient/Controllers/GrpcController.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcClient.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GrpcController : ControllerBase
+{
+    [HttpGet]
+    public float Get()
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5219");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = client.Add(new CalcRequest { A = 5, B = 10 });
+
+        return reply.Result;
+    }
+}
