44ceec6824062380df1f653431bb6ce5a53870fc
Pashaie <pashaie@gmail.com>
2025-03-10 20:28:26 +0330

    crud

--- RestAPI/Controllers/UsersController.cs
+++ RestAPI/Controllers/UsersController.cs
@@ -13,4 +13,49 @@ public class UsersController : ControllerBase
     {
         return Program.Users;
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<User> Get(int id)
+    {
+        var user = Program.Users.FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return user;
+    }
+
+    [HttpPost]
+    public ActionResult<int> Post([FromBody] User user ) {
+        user.Id = Program.Users.Max(x => x.Id) + 1;
+        Program.Users.Add(user);
+        return user.Id;
+    }
+
+
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody] User updatedUser)
+    {
+        var user = Program.Users.FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        user.Name = updatedUser.Name;
+        user.Email = updatedUser.Email;
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var user = Program.Users.FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        Program.Users.Remove(user);
+        return NoContent();
+    }
 }
